0c7db58e7733c8b394c9126795b957cf31e5ee4f
Mike <mike_griffin@comcast.net>
2022-08-30 20:24:05 -0700

    Initial Commit

---
+++ CoolViews/CoolViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolViewsApp.swift
+//  CoolViews
+//
+//  Created by Mike Griffin on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
