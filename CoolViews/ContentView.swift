3a429d1324215f9548102b4433880de6a9482bae
Mike <mike_griffin@comcast.net>
2022-08-30 20:31:25 -0700

    Added README

--- CoolViews/ContentView.swift
+++ CoolViews/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                NavigationLink {
+                    SideScrollScreenView()
+                } label: {
+                    Text("Side scroll")
+                }
+
+            }
+        }
     }
 }
 
